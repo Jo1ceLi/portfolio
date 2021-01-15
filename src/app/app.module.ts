6f6a890516471dbd783b51894e1451d014394843
Jo1ceLi <ms0615122@gmail.com>
2021-01-15 10:39:52 +0800

    Angular material Fixes #16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -17,6 +16,7 @@ import { PorSPipe } from './por-s.pipe';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PositionComponent } from './position/position.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -38,7 +38,9 @@ import { PositionComponent } from './position/position.component';
     RouterModule,
     AppRoutingModule,
 
-    HttpClientModule
+    HttpClientModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
