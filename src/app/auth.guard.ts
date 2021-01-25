877ece0737370e191cfa4e0ff9b4d02c79d0d2d4
Jo1ceLi <ms0615122@gmail.com>
2021-01-25 21:29:02 +0800

    redirect to login if not authenticate

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -17,9 +17,7 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if (localStorage.getItem('access_token')){
-        // return true;
         const JWT = localStorage.getItem('access_token');
-        // console.log('got access', JWT);
         return this.http.post('https://basic-dispatch-298807.df.r.appspot.com/verify', {access_token: JWT})
         .pipe(map((res) => {
           if (res){
@@ -32,33 +30,9 @@ export class AuthGuard implements CanActivate {
             return false;
           }
         }));
-
-
-        // .pipe(map((res: {authenticated: boolean}) => {
-        //   if (res.authenticated === true){
-        //     return true;
-        //   }
-        // }) );
-
-
-        // tslint:disable-next-line: object-literal-key-quotes
-        // this.http.post('http://localhost:8080/verify', {'access_token': access_toekn})
-        // .pipe(
-        //   map((res) => true )
-        // );
       }else{
-        console.log('else');
+        this.router.navigateByUrl('/login');
         return false;
       }
-
     }
-    // .subscribe((res) => {
-    //   console.log(res);
-    //   return true;
-    // }, (err) => {
-    //   console.log(err);
-    //   console.log(state);
-    //   return this.router.navigateByUrl('/login?returnUrl=' + state.url);
-    // });
-
 }
