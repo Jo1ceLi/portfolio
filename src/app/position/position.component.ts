314f825bc203a18c7b62b5f16d6f9a29829e7a91
Jo1ceLi <ms0615122@gmail.com>
2021-02-02 21:46:24 +0800

    fix position & cash

--- src/app/position/position.component.ts
+++ src/app/position/position.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataTableDirective } from 'angular-datatables';
 import { data } from 'jquery';
+import * as moment from 'moment';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -15,26 +16,38 @@ export class PositionComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
   positionDatas;
+  cashDatas;
   lastClosingPriceDatas;
   constructor(private http: HttpClient) { }
 
-  positionSum;
+  positionSum: number;
   getSum(datas): number {
     let sum = 0;
     // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < datas.length; i++ ){
-      if (i < datas.length - 1){
-        sum += (datas[i].cost * datas[i].amount);
-      }else{
-        sum += datas[i].closingprice;
-      }
-
+        sum += (datas[i].closingprice * datas[i].amount);
     }
     return sum;
   }
-  // tslint:disable-next-line: typedef
-  private async fetchDataFromHTTP() {
-    await this.http.get(`https://basic-dispatch-298807.df.r.appspot.com/api/closingprice/2021-01-15`)
+
+  private getLastBusinessDay(): Date {
+    const workday = moment();
+    const day = workday.day();
+    let diff = 1;  // returns yesterday
+    if (day === 0 || day === 1){  // is Sunday or Monday
+      diff = day + 2;  // returns Friday
+    }
+    return workday.subtract(diff, 'days').toDate();
+  }
+
+  private async fetchDataFromHTTP(): Promise<void>  {
+    const lastBusinessDate = this.getLastBusinessDay().toJSON().slice(0, 10);
+    await this.http.get('https://basic-dispatch-298807.df.r.appspot.com/api/cash/')
+    .toPromise()
+    .then(res => {
+      this.cashDatas = res;
+    });
+    await this.http.get(`https://basic-dispatch-298807.df.r.appspot.com/api/closingprice/${lastBusinessDate}`)
     .toPromise()
     .then(res => {
       this.lastClosingPriceDatas = res;
@@ -50,18 +63,14 @@ export class PositionComponent implements OnInit {
           }
         });
       });
-      this.positionDatas.push({symbol: 'CASH', closingprice: 14778, amount: 1});
       this.positionSum = this.getSum(this.positionDatas);
+      this.positionSum += this.cashDatas[0].amount;
+      this.positionDatas.push({symbol: 'CASH', closingprice: this.cashDatas[0].amount, amount: 1});
       this.dtTrigger.next();
     });
   }
 
   ngOnInit(): void {
-    const today = new Date();
-    today.setDate(today.getDate() - 1);
-    const todayString = (today.toISOString().slice(0, 10));
-    console.log(todayString);
-
     this.fetchDataFromHTTP();
   }
 
